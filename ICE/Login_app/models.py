3ef4d0bb072e49979aeb11e158dff1bc1a691064
arman <armanice09@gmail.com>
2020-05-12 02:03:31 +0600

    project upload

---
+++ ICE/Login_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+#username,email,pass,first_name,last_name
+
+#extra kno field create koror jonno
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    facebook_id = models.URLField(blank = True)
+
+    profile_pic = models.ImageField(upload_to = 'profile_pics', blank = True)
+
+
+
+    def __str__(self):
+        return self.user.username
